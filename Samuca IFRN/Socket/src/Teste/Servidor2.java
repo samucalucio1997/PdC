be7a67da14c37d806ebd97576e9e9db0257e3a69
Samuel Lúcio <samucalucio@hotmail.com>
2023-03-24 00:05:42 -0300

    Lá vai mais um

--- Samuca IFRN/Socket/src/Teste/Servidor2.java
+++ Samuca IFRN/Socket/src/Teste/Servidor2.java
@@ -7,14 +7,17 @@ import java.nio.channels.ServerSocketChannel;
 
 public class Servidor2 {
 	
- public static void main(String[] args) {
-	
+ public static void main(String[] args) throws IOException, ClassNotFoundException {
+	 Servidor2 server = new Servidor2();
+	 server.recebeObj();
 }
  private java.nio.channels.SocketChannel SocketChannel=null;
- void recebeObj() throws IOException {
+ void recebeObj() throws IOException, ClassNotFoundException {
 	 SocketChannel = SocketChannel();
 	 ObjectInputStream obj = new ObjectInputStream(SocketChannel.socket().getInputStream());
 	 //agora o Syscalls(Objeto) receberá o retorno do metodo read(). Para isso, a instancia obj chama o método readObject()
+	 String novo = (String) obj.readObject();
+	 System.out.println("Olha aí" + novo);
  }
  
  private java.nio.channels.SocketChannel SocketChannel() throws IOException {
--- Samuca IFRN/Socket/src/Teste/proSock.java
+++ Samuca IFRN/Socket/src/Teste/proSock.java
@@ -1,21 +1,36 @@
 package Teste;
 
 
-import java.io.FileOutputStream;
+//import java.io.DataInputStream;
+//import java.io.DataOutputStream;
+//import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
  
 
 public class proSock {
 	public static void main(String[] args) throws IOException {
-		Socket novo = new Socket("45.228.102.255",4422);
-		FileOutputStream fos = new FileOutputStream("C:\\Users\\Davi Lúcio\\Documents\\Samuca IFRN\\minop.txt");
-		ObjectOutputStream dado = new ObjectOutputStream(fos);
-        dado.writeObject("Olá");//o object seria o syscall
-        dado.close();
-        novo.close();
+		Scanner tec = new Scanner(new FileReader("C:\\Users\\Davi Lúcio\\Documents\\Samuca IFRN\\minop.txt"));
+		
+Socket novo = new Socket("192.168.0.111",4511);
+		ObjectOutputStream dado = new ObjectOutputStream(novo.getOutputStream());
+//        dado.writeObject("C:\\Users\\Davi Lúcio\\Documents\\Samuca IFRN\\minop.txt");
+        //o object seria o syscall
+		ObjectInputStream volta = new ObjectInputStream(novo.getInputStream());
+		while(tec.hasNext()) {
+			dado.writeUTF(tec.nextLine());
+			System.out.println(volta.readUTF());
+		}
+
+		
+            novo.close();
+            dado.close();
+    		tec.close();
 	   	}
 	
 
